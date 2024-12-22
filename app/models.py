214bf92d1134c465e6c3708565716b945684d4c5
muni1817 <munigumma1817@gmail.com>
2024-12-22 04:53:07 -0800

    first commit

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Country(models.Model):
+    c_id=models.IntegerField(primary_key=True)
+    c_name=models.CharField(max_length=100)
+class Capital(models.Model):
+    cap_id=models.IntegerField(primary_key=True)
+    cap_name=models.CharField(max_length=100)
+    c_id=models.ForeignKey(Country,on_delete=models.CASCADE)
